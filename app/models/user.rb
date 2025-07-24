bee2acd7000113f83d779673a61f3135e9b61d1a
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-07-24 18:28:32 -0300

    create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :api
 
   has_many :orders, dependent: :destroy
-
-  validates :name, :email, presence: true
   validates :email, uniqueness: true
 end
