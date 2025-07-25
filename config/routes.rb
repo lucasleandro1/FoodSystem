47a932b0f30fe0fcd05546b11cc44cfc334c21c8
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-07-25 16:45:10 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
-      resources :orders
-      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-      # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-      # Can be used by load balancers and uptime monitors to verify that the app is live.
+      # Rotas para restaurantes (Index e Show)
+      resources :restaurants, only: [ :index, :show ] do
+        # Rotas para visualizar os produtos de cada restaurante
+        resources :products, only: [ :index, :show ]
+      end
+
+      # Rotas para pedidos (Create e Index)
+      resources :orders, only: [ :create, :index ]
+
+      # Reveal health status on /up that returns 200 if the app boots with no exceptions
+
       get "up" => "rails/health#show", as: :rails_health_check
-
-      # Defines the root path route ("/")
-      # root "posts#index"
     end
   end
 end
